0b3efec00f66ffd4fc66a9ffc15567fa77f620fa
esp-control <mohamadafif9408@gmail.com>
2025-05-05 11:12:01 +0700

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8_pj7lvjfbKxY10-JWPEr7kx79j0SNKg",
+    authDomain: "esp-first-bf7a1.firebaseapp.com",
+    projectId: "esp-first-bf7a1",
+    appId: "1:734784429393:web:2a047d08d5b3d93d1fc770",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
